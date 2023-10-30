d61e89191843399986ddb3029bec1807f74a07b8
nicolewiktor <nicole.wiktor@outlook.com>
2023-10-30 17:45:57 -0400

    Monster following character

--- Underlight/Assets/Scripts/Monster_Controller.cs
+++ Underlight/Assets/Scripts/Monster_Controller.cs
@@ -5,19 +5,28 @@ using UnityEngine.SceneManagement;
 
 public class Monster_Controller : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   [SerializeField]
+    private GameObject player; 
+    public float followSpeed = 2.0f; 
+    public float safeDistance = 2.0f; 
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Calculate the distance between AngrySpooder and character
+        float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
+
+        // If the distance is greater than the safe distance, start following
+        if (distanceToPlayer > safeDistance)
+        {
+            // Determine the direction and move AngrySpooder towards the player
+            Vector3 moveDirection = (player.transform.position - transform.position).normalized;
+            transform.position += moveDirection * followSpeed * Time.deltaTime;
+        }
     }
-    private void OnCollisionEnter2D(Collision2D col){
-        print("colission");
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        print("collision");
         SceneManager.LoadScene("GameOver");
     }
 }
