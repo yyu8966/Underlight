ba7cbec32308649ce714a7b0c570089bc683f893
nicolewiktor <nicole.wiktor@outlook.com>
2023-12-02 19:10:41 -0500

    Setting up code for AI

--- Underlight/Assets/Scripts/Monster_Controller.cs
+++ Underlight/Assets/Scripts/Monster_Controller.cs
@@ -5,30 +5,78 @@ using UnityEngine.SceneManagement;
 
 public class Monster_Controller : MonoBehaviour
 {
-   [SerializeField]
-    private GameObject player; 
-    public float followSpeed = 2.0f; 
-    public float safeDistance = 2.0f; 
+    [SerializeField]
+    private GameObject player;
+    public float followSpeed = 2.0f;
+    public float safeDistance = 2.0f;
+
+    // Variables for AI progression
+    private int itemsCollectedByPlayer = 0;
+    private bool usePathfinding = false;
+    private bool useHiddenPaths = false;
 
     void Update()
     {
-        // Calculate the distance between AngrySpooder and character
+        // Update AI behavior based on items collected
+        UpdateAIBehavior();
+
+        // Calculate the distance between monster and player
         float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
 
-        // If the distance is greater than the safe distance, start following
         if (distanceToPlayer > safeDistance)
         {
-            // Determine the direction and move AngrySpooder towards the player
-            Vector3 moveDirection = (player.transform.position - transform.position).normalized;
-            transform.position += moveDirection * followSpeed * Time.deltaTime;
+            if (usePathfinding)
+            {
+                // Use pathfinding to move towards the player
+                MoveUsingPathfinding();
+            }
+            else
+            {
+                // Directly move towards the player
+                Vector3 moveDirection = (player.transform.position - transform.position).normalized;
+                transform.position += moveDirection * followSpeed * Time.deltaTime;
+            }
         }
     }
 
     private void OnCollisionEnter2D(Collision2D col)
     {
-        if(col.gameObject.tag == "Player"){
-        print("collision");
-        SceneManager.LoadScene("GameOver");
+        if (col.gameObject.tag == "Player")
+        {
+            print("collision");
+            SceneManager.LoadScene("GameOver");
         }
+    }
+
+    private void UpdateAIBehavior()
+    {
+        // Adjust AI behavior based on items collected
+        // Placeholder for item collection check
+        itemsCollectedByPlayer = 0/* Get collected items count */;
+
+        // Increase speed based on collected items
+        followSpeed = 2.0f + itemsCollectedByPlayer * 0.1f;
+
+        // Enable pathfinding after a certain number of items collected
+        if (itemsCollectedByPlayer > 10)
+        {
+            usePathfinding = true;
+        }
+
+        // Enable use of hidden paths at later stages
+        if (itemsCollectedByPlayer > 20)
+        {
+            useHiddenPaths = true;
+        }
+    }
+
+    private void MoveUsingPathfinding()
+    {
+        // Implement pathfinding logic here
+    }
+
+    private void UseHiddenPaths()
+    {
+        // Implement logic for using hidden pathways
     }
 }
